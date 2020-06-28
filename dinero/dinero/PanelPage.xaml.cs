11a1710033aa0afa245c7fd24e54bc2877d6f75d
BeaverJulia <jgula@edu.cdv.pl>
2020-06-28 21:44:17 +0200

    #details looking better

--- dinero/dinero/PanelPage.xaml.cs
+++ dinero/dinero/PanelPage.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+using System.Globalization;
 using dinero.Models;
-using GalaSoft.MvvmLight.Command;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,9 +10,10 @@ namespace dinero
     [XamlCompilation(XamlCompilationOptions.Skip)]
     public partial class PanelPage : ContentPage
     {
+        private TransactionView TransactionView;
         public List<Wallet> WalletsList;
         private WalletView WalletView;
-        private TransactionView TransactionView;
+
         public PanelPage()
         {
             InitializeComponent();
@@ -28,7 +22,6 @@ namespace dinero
             Transactions.ItemsSource = GetTransactions();
             Wallets.ItemsSource = WalletsList;
             btnNewTransaction.Clicked += BtnNewTransaction_Clicked;
-            
         }
 
         private async void BtnNewTransaction_Clicked(object sender, EventArgs e)
@@ -44,22 +37,21 @@ namespace dinero
 
         public List<Transaction> GetTransactions()
         {
-            
             TransactionView = new TransactionView();
             var transactions = TransactionView.GetRequests(10).Result;
             foreach (var transaction in transactions)
             {
-                var dateTime = DateTimeOffset.Parse(transaction.Paid_At, System.Globalization.CultureInfo.InvariantCulture);
+                var dateTime = DateTimeOffset.Parse(transaction.Paid_At, CultureInfo.InvariantCulture);
                 transaction.Paid_At = dateTime.DateTime.ToLongDateString();
             }
+
             return transactions;
         }
 
         public async void GetWalletDetails()
         {
-            var walletDetail = (Wallet)Wallets.SelectedItem;
+            var walletDetail = (Wallet) Wallets.SelectedItem;
             await Navigation.PushModalAsync(new WalletDetailsPage(walletDetail));
-
         }
 
         public async void GetTransactionDetails()
@@ -67,15 +59,15 @@ namespace dinero
             var transactionDetail = (Transaction) Transactions.SelectedItem;
             await Navigation.PushModalAsync(new TransactionDetails(transactionDetail));
         }
-        private void Tapped(object sender, ItemTappedEventArgs e)
-        {
-            GetWalletDetails();
-        }
 
         private void Transactions_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             GetTransactionDetails();
         }
 
+        private void Wallets_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            GetWalletDetails();
+        }
     }
-    }
+}
